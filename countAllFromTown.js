2c5ef3283d3db912e585a2e38fa50b4c785333b4
HopeLotriet <hopelotriet@gmail.com>
2023-07-19 11:31:48 +0200

    commit read me file

--- countAllFromTown.js
+++ countAllFromTown.js
@@ -9,5 +9,5 @@ export default function countAllFromTown (string, regNum){
     return count.length;
   }
     
-    console.log(countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL'));
+  console.log(countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL'));
   console.log(countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341','CF'));
---
+++ readme.md
@@ -0,0 +1,1 @@
+[![Node.js CI](https://github.com/HopeLotriet/bootcamp-terminal-tests/actions/workflows/node.js.yml/badge.svg)](https://github.com/HopeLotriet/bootcamp-terminal-tests/actions/workflows/node.js.yml)
